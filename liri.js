3477079e7b4203433008368fae7ce4acb14f682b
Samantha Covington <samanthanc_08@yahoo.com>
2019-10-14 22:25:44 -0500

    errors fixed

--- liri.js
+++ liri.js
@@ -36,70 +36,68 @@ function runLiri() {
 }
 
 //working code
-function showConcertInfo() {
-    // console.log("concert");
-    var inpoint = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp";
-    axios.get(inpoint).then(function(data) {
-        var bandData = data.data
-            // console.log(bandData);
-        for (var i = 0; i < 5; i++) {
-            var concert = bandData[i];
-            var venue = concert.venue.name;
-            var city = concert.venue.city;
-            var country = concert.venue.country;
-            var date = moment(concert.datetime).format("MM/DD/YYYY");
-            console.log(venue, city, country, date);
+// function showConcertInfo() {
+//     // console.log("concert");
+//     var inpoint = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp";
+//     axios.get(inpoint).then(function(data) {
+//         var bandData = data.data;
+//         // console.log(bandData);
+//         for (var i = 0; i < 5; i++) {
+//             var concert = bandData[i];
+//             var venue = concert.venue.name;
+//             var city = concert.venue.city;
+//             var country = concert.venue.country;
+//             var date = moment(concert.datetime).format("MM/DD/YYYY");
+//             console.log(venue, city, country, date);
 
-        }
-    })
+//         }
+//     })
 
-}
+// }
 
 
-
+// working code
 function showMovieInfo() {
-    var inpoint = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
-    axios.get(inpoint).then(function(data) {
-                if (userInput === undefined) userInput = "Mr. Nobody"
-                var movieData = data.data
-                console.log(movieData);
-                // for (var i = 0; i < 5; i++) {
-                //     var title = movieData[i];
-                //         var year = concert.venue.name;
-                //         var rating = concert.venue.city;
-                //         var rottonRating = concert.venue.country;
-                //         var country = ;
-                //         var language = ;
-                //         var plot = ;
-                //         var actors = ;
-                //         console.log(title, year, rating, rottonRating, country, language, plot, actors);
+    if (userInput === undefined) {
+        userInput = "Mr. Nobody"
+        console.log("If you haven't watched Mr.Nobody then you should: http://www.imdb.com/title/tt0485947/");
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&apikey=trilogy";
+    axios.get(queryUrl).then(function(response) {
 
-                //     }
-
-
-                //     console.log(data);
-
-                // };
+        var Title = ("Title: " + response.data.Title);
+        var Year = ("Year of release: " + response.data.Year);
+        var imbdRating = ("IMDB rating: " + response.data.imbdRating);
+        var Rating = (response.data.Ratings[1].Source + " rating: " + response.data.Ratings[1].Value);
+        var Country = ("Country: " + response.data.Country);
+        var Language = ("Language: " + response.data.Language);
+        var Plot = ("Plot: " + response.data.Plot);
+        var Actors = ("Actors: " + response.data.Actors);
+        console.log(Title, Year, imbdRating, Rating, Country, Language, Plot, Actors);
 
 
-                // function showSongInfo() {
-                //     spotify.search({ type: 'track', query: userInput }, function(err, data) {
-                //         if (err) {
-                //             return console.log('Error occurred: ' + err);
-                //         }
-                //         console.log("song");
-                //     })
-                // }
-                // working code
-                function doIt() {
-                    // console.log("Do it");
-                    fs.readFile("random.txt", "utf8", function(err, data) {
-                        var newCommand = data.split(",");
-                        userCommands = newCommand[0];
-                        userInput = newCommand[1];
-                        runLiri();
-                    })
+    });
+};
+runLiri();
 
-                }
+// function showSongInfo() {
+//     spotify.search({ type: 'track', query: userInput }, function(err, data) {
+//         if (err) {
+//             return console.log('Error occurred: ' + err);
+//         }
+//         console.log("song");
+//     })
+// }
+// working code
+// function doIt() {
+//     console.log("Do it");
+//     fs.readFile("random.txt", "utf8", function(err, data) {
+//         var newCommand = data.split(",");
+//         userCommands = newCommand[0];
+//         userInput = newCommand[1];
+//         runLiri();
+//     })
 
-                runLiri();
+// }
+
+// runLiri();
